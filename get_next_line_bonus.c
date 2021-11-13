e49fa8341fcf827b57c5b457b1b109c3cc79b9f4
eestelle <yosiky@list.ru>
2021-11-13 21:42:36 +0300

    fix less problem

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,96 +1,183 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: eestelle <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/23 15:58:01 by eestelle          #+#    #+#             */
-/*   Updated: 2021/10/23 17:28:24 by eestelle         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-int	ft_check(char *buff, size_t start, size_t finish)
-{
-	size_t	len;
-
-	len = 0;
-	while (start + len < finish && buff[start + len] != '\n')
-		len++;
-	if (buff[start + len] == '\n')
-		return (len + 1);
-	else
-		return (-1);
-}
-
-void	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+char	*ft_memcpy(char *s1, char *s2, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < dstsize - 1 && dstsize > 0)
+	while (i < len)
 	{
-		dst[i] = src[i];
+		*(s1++) = *(s2++);
 		i++;
 	}
-	if (dstsize > 0)
-		dst[i] = '\0';
+	return (s1);
 }
 
-char	*ft_arrnew(char **arr, size_t size)
+int	ft_init(int fd, t_buff *buff, t_lst **lbegin, t_lst ***lend)
 {
-	*arr = (char *)malloc(sizeof(char) * (size + 1));
-	return (*arr);
+	if (fd < 0 || fd > FOPEN_MAX)
+		return (1);
+	if (BUFFER_SIZE <= 0)
+		return (1);
+	if (buff->data == NULL)
+	{
+		buff->data = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		buff->size = 0;
+	}
+	if (buff->data == NULL)
+		return (1);
+	*lbegin = NULL;
+	*lend = lbegin;
+	return (0);
+}
+
+size_t	ft_lstlen(t_lst *lst)
+{
+	size_t	len;
+
+	len = 0;
+	while (lst != NULL)
+	{
+		len++;
+		lst = lst->next;
+	}
+	return (len);
+}
+
+t_lst	*ft_lstnew(char **buff, size_t len)
+{
+	t_lst	*elem;
+
+	elem = (t_lst *)malloc(sizeof(t_lst));
+	if (elem == NULL)
+		return (NULL);
+	elem->data = *buff;
+	*buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (*buff == NULL)
+	{
+		free(elem->data);
+		free(elem);
+		return (NULL);
+	}
+	elem->len = len;
+	elem->next = NULL;
+	return (elem);
+}
+
+void	ft_lstclear(t_lst *lst)
+{
+	t_lst	*iter;
+
+	while (lst != NULL)
+	{
+		free(lst->data);
+		iter = lst;
+		lst = lst->next;
+		free(iter);
+	}
+}
+
+char	*ft_lsttoarr(t_lst *lst)
+{
+	char	*res;
+	char	*iter;
+	size_t	count;
+	t_lst	*start;
+
+	res = NULL;
+	start = lst;
+	count = ft_lstlen(lst);
+	if (count != 0)
+	{
+		res = (char *)malloc(sizeof(char) * (BUFFER_SIZE * count + 1));
+		if (res != NULL)
+		{
+			iter = res;
+			while (lst != NULL)
+			{
+				iter = ft_memcpy(iter, lst->data, lst->len);
+				lst = lst->next;
+			}
+			*iter = '\0';
+		}
+	}
+	ft_lstclear(start);
+	return (res);
+}
+
+void	ft_clear(t_buff *buff, t_lst *begin)
+{
+	if (buff->data != NULL)
+	{
+		free(buff->data);
+		buff->data = NULL;
+	}
+	buff->size = 0;
+	if (begin != NULL)
+		ft_lstclear(begin);
+}
+
+int	ft_check(t_buff *buff, size_t *len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < buff->size)
+	{
+		if (buff->data[i] == '\n')
+		{
+			*len = i + 1;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_todo(t_buff *buff, t_lst ***end, t_lst *begin)
+{
+	char	flag;
+	size_t	len;
+
+	flag = ft_check(buff, &len);
+	if (flag)
+		**end = ft_lstnew(&buff->data, len);
+	else
+		**end = ft_lstnew(&buff->data, buff->size);
+	if (**end == NULL)
+	{
+		ft_lstclear(begin);
+		return (-1);
+	}
+	if (flag)
+	{
+		buff->size -= len;
+		ft_memcpy(buff->data, (**end)->data + len, buff->size);
+		return (1);
+	}
+	*end = &(**end)->next;
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static	t_buff	abuff[1024] = {};
-	char			*result;
-	int				len;
-	size_t			alen;
-	size_t          count;
+	static	t_buff	buff[FOPEN_MAX] = {};
+	t_lst			*lst_begin;
+	t_lst			**lst_end;
 
-	if (fd < 0 || fd > 1024)
+	if (ft_init(fd, &buff[fd], &lst_begin, &lst_end))
 		return (NULL);
-	alen = BUFFER_SIZE;
-	count = 0;
-	if (ft_arrnew(&result, alen) == NULL)
-		return (NULL);
-	if (abuff[fd].buff == NULL)
+	if (buff[fd].size != 0)
+		if (ft_todo(&buff[fd], &lst_end, lst_begin))
+			return (ft_lsttoarr(lst_begin));
+	while (1)
 	{
-		if (ft_arrnew(&abuff[fd].buff, BUFFER_SIZE - 1) == NULL)
-		{
-			free(result);
-			return (NULL);
-		}
+		buff[fd].size = read(fd, buff[fd].data, BUFFER_SIZE);
+		if (buff[fd].size == (size_t)-1)
+			ft_clear(&buff[fd], lst_begin);
+		if (buff[fd].size <= 0 || buff[fd].size == (size_t)-1
+			|| ft_todo(&buff[fd], &lst_end, lst_begin))
+			break;
 	}
-	else
-	{
-		len = ft_check(abuff[fd].buff, abuff[fd].begin, abuff[fd].end);
-		if (len >= 0)
-		{
-			ft_strlcpy(result, abuff[fd].buff + abuff[fd].begin, len + 1);
-			if (abuff[fd].begin + len == abuff[fd].end)
-			{
-				abuff[fd].begin = 0;
-				abuff[fd].end = 0;
-				free(abuff[fd].buff);
-				abuff[fd].buff = NULL;
-			}
-			else
-				abuff[fd].begin += len;
-			return (result);
-		}
-		else
-		{
-			ft_strlcpy(result, abuff[fd].buff + abuff[fd].begin, abuff[fd].end + 1);
-			count = abuff[fd].end - abuff[fd].begin;
-			abuff[fd].begin = 0;
-			abuff[fd].end = 0;
-		}
-	}
-	return (ft_read(fd, result, alen, count, &abuff[fd]));
+	return (ft_lsttoarr(lst_begin));
 }
